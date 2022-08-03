8d26233d12f3a4194252ac65e54688f713b00adb
Korawit Wassanarungruengsuk <korawit.was@gmail.com>
2022-08-03 17:38:31 +0700

    add menuFlex

--- handler/init.go
+++ handler/init.go
@@ -116,33 +116,147 @@ var (
 		"type": "bubble",
 		"hero": {
 		  "type": "image",
-		  "url": "https://www.i-pic.info/i/KMdp196143.png",
+		  "url": "https://www.img.in.th/images/a4609b9e99ef2a28a9c55ebd26935b34.png",
 		  "size": "full",
-		  "aspectRatio": "20:13",
-		  "aspectMode": "cover"
+		  "aspectRatio": "24:12",
+		  "aspectMode": "cover",
+		  "action": {
+			"type": "uri",
+			"label": "Action",
+			"uri": "https://linecorp.com/"
+		  }
 		},
 		"body": {
 		  "type": "box",
-		  "layout": "vertical",
+		  "layout": "horizontal",
+		  "spacing": "md",
 		  "contents": [
 			{
-			  "type": "text",
-			  "text": "Booking",
-			  "weight": "bold",
-			  "size": "xl",
-			  "contents": []
+			  "type": "box",
+			  "layout": "vertical",
+			  "flex": 1,
+			  "margin": "xs",
+			  "contents": [
+				{
+				  "type": "image",
+				  "url": "https://www.img.in.th/images/316d54a84bd0be0e0447dbb7e75dade7.png",
+				  "size": "sm",
+				  "aspectRatio": "4:3"
+				},
+				{
+				  "type": "image",
+				  "url": "https://www.img.in.th/images/889028fcf2d1509b15db5424220bd8c8.png",
+				  "margin": "md",
+				  "size": "sm",
+				  "aspectRatio": "4:3"
+				},
+				{
+				  "type": "image",
+				  "url": "https://www.img.in.th/images/896c92859ab109e7de86954e8fccf68c.png",
+				  "margin": "md",
+				  "size": "sm",
+				  "aspectRatio": "4:3"
+				},
+				{
+				  "type": "image",
+				  "url": "https://www.img.in.th/images/a11597730e66ab8073de518442ee0983.png",
+				  "margin": "md",
+				  "size": "sm",
+				  "aspectRatio": "4:3"
+				},
+				{
+				  "type": "image",
+				  "url": "https://www.img.in.th/images/73a965e125e523fae04685fc2569feb2.png",
+				  "margin": "md",
+				  "size": "sm",
+				  "aspectRatio": "4:3"
+				}
+			  ]
 			},
 			{
 			  "type": "box",
-			  "layout": "baseline",
-			  "spacing": "sm",
+			  "layout": "vertical",
+			  "flex": 2,
+			  "spacing": "lg",
 			  "contents": [
 				{
 				  "type": "text",
-				  "text": "How many people are you?",
-				  "size": "lg",
-				  "color": "#AAAAAA",
-				  "flex": 1,
+				  "text": "RELX",
+				  "weight": "regular",
+				  "size": "xl",
+				  "flex": 2,
+				  "align": "center",
+				  "gravity": "center",
+				  "action": {
+					"type": "message",
+					"text": "RELX"
+				  },
+				  "contents": []
+				},
+				{
+				  "type": "separator"
+				},
+				{
+				  "type": "text",
+				  "text": "INFY",
+				  "weight": "regular",
+				  "size": "xl",
+				  "flex": 2,
+				  "align": "center",
+				  "gravity": "center",
+				  "action": {
+					"type": "message",
+					"text": "INFY"
+				  },
+				  "contents": []
+				},
+				{
+				  "type": "separator"
+				},
+				{
+				  "type": "text",
+				  "text": "JUES",
+				  "size": "xl",
+				  "flex": 2,
+				  "align": "center",
+				  "gravity": "center",
+				  "action": {
+					"type": "message",
+					"text": "JUES"
+				  },
+				  "contents": []
+				},
+				{
+				  "type": "separator"
+				},
+				{
+				  "type": "text",
+				  "text": "7-11",
+				  "size": "xl",
+				  "flex": 2,
+				  "align": "center",
+				  "gravity": "center",
+				  "action": {
+					"type": "message",
+					"text": "7-11"
+				  },
+				  "contents": []
+				},
+				{
+				  "type": "separator"
+				},
+				{
+				  "type": "text",
+				  "text": "BOLD",
+				  "weight": "regular",
+				  "size": "xl",
+				  "flex": 2,
+				  "align": "center",
+				  "gravity": "center",
+				  "action": {
+					"type": "message",
+					"text": "BOLD"
+				  },
 				  "contents": []
 				}
 			  ]
@@ -151,100 +265,15 @@ var (
 		},
 		"footer": {
 		  "type": "box",
-		  "layout": "vertical",
-		  "flex": 0,
-		  "spacing": "sm",
+		  "layout": "horizontal",
 		  "contents": [
 			{
-			  "type": "box",
-			  "layout": "vertical",
+			  "type": "button",
 			  "action": {
 				"type": "message",
-				"label": "Alone",
-				"text": "Alone"
-			  },
-			  "height": "40px",
-			  "backgroundColor": "#FBF1C2FF",
-			  "cornerRadius": "8px",
-			  "contents": [
-				{
-				  "type": "text",
-				  "text": "Alone",
-				  "color": "#000000FF",
-				  "align": "center",
-				  "margin": "md",
-				  "contents": []
-				}
-			  ]
-			},
-			{
-			  "type": "box",
-			  "layout": "vertical",
-			  "action": {
-				"type": "message",
-				"label": "Couple",
-				"text": "Couple"
-			  },
-			  "height": "40px",
-			  "backgroundColor": "#DFE9F5",
-			  "cornerRadius": "8px",
-			  "contents": [
-				{
-				  "type": "text",
-				  "text": "Couple",
-				  "color": "#000000FF",
-				  "align": "center",
-				  "margin": "md",
-				  "contents": []
-				}
-			  ]
-			},
-			{
-			  "type": "box",
-			  "layout": "vertical",
-			  "action": {
-				"type": "message",
-				"label": "Small Group (3-4)",
-				"text": "Small Group"
-			  },
-			  "height": "40px",
-			  "backgroundColor": "#F6E6DE",
-			  "cornerRadius": "8px",
-			  "contents": [
-				{
-				  "type": "text",
-				  "text": "Small Group (3-4)",
-				  "color": "#000000FF",
-				  "align": "center",
-				  "margin": "md",
-				  "contents": []
-				}
-			  ]
-			},
-			{
-			  "type": "box",
-			  "layout": "vertical",
-			  "action": {
-				"type": "message",
-				"label": "The Gang (5-6)",
-				"text": "The Gang"
-			  },
-			  "height": "40px",
-			  "backgroundColor": "#D9F0E7",
-			  "cornerRadius": "8px",
-			  "contents": [
-				{
-				  "type": "text",
-				  "text": "The Gang (5-6)",
-				  "color": "#000000FF",
-				  "align": "center",
-				  "margin": "md",
-				  "contents": []
-				}
-			  ]
-			},
-			{
-			  "type": "spacer"
+				"label": "See all",
+				"text": "All Flavor"
+			  }
 			}
 		  ]
 		}
--- handler/line_handler.go
+++ handler/line_handler.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -33,60 +34,22 @@ func (h goodHandler) Callback(c *gin.Context) {
 						log.Print(err)
 					}
 				}
-				if message.Text == "ยกเลิกคิว" {
-					// good, err := h.qService.DeleteGoodbyUID(event.Source.UserID)
+				if message.Text == "Flavor" {
+					// Unmarshal JSON
+					flexContainer, err := linebot.UnmarshalFlexMessageJSON([]byte(MenuFlex))
 					if err != nil {
-						if err.Error() == "user Code not found" {
-							if _, err := bot.PushMessage(event.Source.UserID, linebot.NewTextMessage("ท่านยังไม่ได้จองคิวไม่สามารถยกเลิกได้")).Do(); err != nil {
-								log.Print(err)
-							}
-							return
-						} else {
-							if _, err := bot.PushMessage(event.Source.UserID, linebot.NewTextMessage("เกิดข้อผิดพลาดไม่สามารถยกเลิกคิวได้")).Do(); err != nil {
-								log.Print(err)
-							}
-							return
-						}
+						log.Println(err)
 					}
-					// if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(fmt.Sprintf("ท่านยกเลิกคิว %v เรียบร้อยแล้ว", good.Code))).Do(); err != nil {
-					// log.Print(err)
-					// }
+					fmt.Println(flexContainer)
+					// New Flex Message
+					flexMessage := linebot.NewFlexMessage(message.Text, flexContainer)
+					// Reply Message
+					_, err = bot.ReplyMessage(event.ReplyToken, flexMessage).Do()
+					if err != nil {
+						log.Print(err)
+					}
 					return
 				}
-
-				// split := strings.Split(message.Text, " ")
-				// if split[0] == "ดู" || split[0] == "ตรวจสอบ" || split[0] == "ค้นหา" {
-				// 	fmt.Println(split[1])
-				// 	flex, err := h.qService.FlexGood(split[1])
-				// 	fmt.Println(err)
-				// 	if err != nil {
-				// 		if err.Error() == "repository error" {
-				// 			if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("ไม่พบเลขคิวที่คุณค้นหาหรืออาจเลยคิวของคุณมาแล้ว")).Do(); err != nil {
-				// 				log.Print(err)
-				// 				return
-				// 			}
-				// 			return
-				// 		} else {
-				// 			if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("ระบบผิดพลาด")).Do(); err != nil {
-				// 				log.Print(err)
-				// 				return
-				// 			}
-				// 			return
-				// 		}
-				// 	}
-				// 	// Unmarshal JSON
-				// 	flexContainer, err := linebot.UnmarshalFlexMessageJSON([]byte(flex))
-				// 	if err != nil {
-				// 		log.Println(err)
-				// 	}
-				// 	// New Flex Message
-				// 	flexMessage := linebot.NewFlexMessage(split[1], flexContainer)
-				// 	// Reply Message
-				// 	_, err = bot.ReplyMessage(event.ReplyToken, flexMessage).Do()
-				// 	if err != nil {
-				// 		log.Print(err)
-				// 	}
-				// } else {
 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("ขออภัยครับ แต่เรายังไม่เข้าใจ ท่านอยากจะทวนอีกรอบหรือส่งต่อให้เจ้าหน้าที่ตอบคำถามดีครับ")).Do(); err != nil {
 					log.Print(err)
 				}
